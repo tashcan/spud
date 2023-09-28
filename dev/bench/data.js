84e52f9d455b2faa748645cd4a14a0acdd34b435
github-action-benchmark <github@users.noreply.github.com>
2023-09-28 19:30:31 +0000

    add Detour Benchmark (catch2) benchmark result for a839f37a624870d2b2ee10f3877c0634dff0f343

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695928834723,
+  "lastUpdate": 1695929431294,
   "repoUrl": "https://github.com/tashcan/spud",
   "entries": {
     "Detour Benchmark": [
@@ -62,6 +62,37 @@ window.BENCHMARK_DATA = {
             "range": "± 1.72611",
             "unit": "us",
             "extra": "100 samples\n3 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "git@tashcan.com",
+            "name": "Tashcan",
+            "username": "tashcan"
+          },
+          "committer": {
+            "email": "git@tashcan.com",
+            "name": "Tashcan",
+            "username": "tashcan"
+          },
+          "distinct": true,
+          "id": "a839f37a624870d2b2ee10f3877c0634dff0f343",
+          "message": "Build and test debug too",
+          "timestamp": "2023-09-28T12:28:03-07:00",
+          "tree_id": "b0d626bf6cfa7c517459a13fd526bea90f7d29ca",
+          "url": "https://github.com/tashcan/spud/commit/a839f37a624870d2b2ee10f3877c0634dff0f343"
+        },
+        "date": 1695929430571,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "test",
+            "value": 19.7916,
+            "range": "± 7.20677",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
           }
         ]
       }
