e24207c9da4efc28072b2664c5603ac696dc79d6
github-action-benchmark <github@users.noreply.github.com>
2023-09-28 19:15:40 +0000

    add Detour Benchmark (catch2) benchmark result for c87f7b17a571572edd14293fd20c62f100b4f50d

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1695928540336,
+  "repoUrl": "https://github.com/tashcan/spud",
+  "entries": {
+    "Detour Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "git@tashcan.com",
+            "name": "Tashcan",
+            "username": "tashcan"
+          },
+          "committer": {
+            "email": "git@tashcan.com",
+            "name": "Tashcan",
+            "username": "tashcan"
+          },
+          "distinct": true,
+          "id": "c87f7b17a571572edd14293fd20c62f100b4f50d",
+          "message": ".",
+          "timestamp": "2023-09-28T12:13:12-07:00",
+          "tree_id": "ec1ac92c1ef60879d09774712e12eb20d0627a0e",
+          "url": "https://github.com/tashcan/spud/commit/c87f7b17a571572edd14293fd20c62f100b4f50d"
+        },
+        "date": 1695928539603,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "test",
+            "value": 17.003,
+            "range": "± 6.52041",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
