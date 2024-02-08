0e95019f7eba9e3669a0ac7b9a49db739c608156
Tashcan <git@tashcan.com>
2024-02-08 15:24:06 -0800

    Add a shared detour test

---
+++ tests/detour/shared/basic.cc
@@ -0,0 +1,33 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <spud/detour.h>
+
+#include <cstdio>
+
+#include <test_util.h>
+
+static bool hook_ran_shared_basic = false;
+static bool condition_intact_for_hook_shared_basic = false;
+
+SPUD_NO_INLINE static void test_function_shared_basic(int n) {
+  if (n == 0) {
+    condition_intact_for_hook_shared_basic = true;
+    return;
+  }
+}
+
+SPUD_NO_INLINE static void hook_shared_basic(auto original, int n) {
+  hook_ran_shared_basic = true;
+  original(0);
+}
+
+// Make sure test_function is not inlined here...this happens on GCC
+static auto *test_function_shared_basic_ptr = &test_function_shared_basic;
+TEST_CASE("Simple Hook with a condition", "[detour:shared:basic]") {
+  hook_ran_shared_basic = false;
+  auto t = SPUD_AUTO_HOOK(test_function_shared_basic, hook_shared_basic);
+  t.install();
+  test_function_shared_basic_ptr(1);
+  REQUIRE(hook_ran_shared_basic == true);
+  REQUIRE(condition_intact_for_hook_shared_basic == true);
+}
---
+++ tests/detour/x86_64/simple/simple.cc
@@ -0,0 +1,98 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <spud/detour.h>
+
+#include <cstdio>
+
+#include <test_util.h>
+
+extern "C" void ASM_FUNC(mov1, ());
+extern "C" bool ASM_NAME(msg);
+
+extern "C" void ASM_FUNC(mov2, (int));
+extern "C" bool ASM_NAME(msg2);
+
+extern "C" void ASM_FUNC(mov3, (int));
+extern "C" bool ASM_NAME(msg3);
+
+extern "C" void ASM_FUNC(mov4, (int));
+extern "C" bool ASM_NAME(msg4);
+
+extern "C" void ASM_FUNC(mov5, (int));
+extern "C" bool ASM_NAME(msg5);
+
+static bool hook_ran_simple_amd64 = false;
+
+SPUD_NO_INLINE static void hook_amd64(auto original, int n) {
+  hook_ran_simple_amd64 = true;
+  original(0);
+}
+
+static void hook_mov1(auto original) {}
+
+TEST_CASE("Simple Hook with a condition asm and removal",
+          "[detour:x64:simple]") {
+  REQUIRE(msg == false);
+  mov1();
+  REQUIRE(msg == true);
+  msg = false;
+  {
+    auto t = SPUD_AUTO_HOOK(mov1, hook_mov1);
+    t.install();
+    mov1();
+    REQUIRE(msg == false);
+  }
+  mov1();
+  REQUIRE(msg == true);
+}
+
+TEST_CASE("Simple Hook with a condition asm mov jump within trampoline",
+          "[detour:x64:simple]") {
+  hook_ran_simple_amd64 = false;
+  auto t = SPUD_AUTO_HOOK(mov2, hook_amd64);
+  t.install();
+  mov2(1);
+  REQUIRE(hook_ran_simple_amd64 == true);
+  REQUIRE(msg2 == true);
+}
+
+TEST_CASE("Simple Hook with a condition asm mov 3", "[detour:x64:simple]") {
+  hook_ran_simple_amd64 = false;
+  auto t = SPUD_AUTO_HOOK(mov3, hook_amd64);
+  t.install();
+  mov3(1);
+  REQUIRE(hook_ran_simple_amd64 == true);
+  REQUIRE(msg3 == true);
+}
+
+TEST_CASE("Simple Hook with a condition asm mov jump within trampoline 2",
+          "[detour:x64:simple]") {
+  hook_ran_simple_amd64 = false;
+  auto t = SPUD_AUTO_HOOK(mov4, hook_amd64);
+  t.install();
+  mov4(0);
+  REQUIRE(hook_ran_simple_amd64 == true);
+  REQUIRE(msg4 == true);
+}
+
+TEST_CASE("Simple Hook with a condition asm mov jump within trampoline 3",
+          "[detour:x64:simple]") {
+  hook_ran_simple_amd64 = false;
+  SPUD_STATIC_DETOUR(mov5, hook_amd64);
+  mov5(0);
+  REQUIRE(hook_ran_simple_amd64 == true);
+  REQUIRE(msg5 == true);
+}
+
+TEST_CASE("Simple Hook lambda mov 4", "[detour:x64:simple]") {
+  hook_ran_simple_amd64 = false;
+  msg4 = false;
+  auto t = spud::create_detour(mov4, [](auto original, int n) {
+    hook_ran_simple_amd64 = true;
+    return original(n);
+  });
+  t.install();
+  mov4(0);
+  REQUIRE(hook_ran_simple_amd64 == true);
+  REQUIRE(msg4 == true);
+}
