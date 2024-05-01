039670260066d0eb6886244f7b4d5ccfabf1daec
Tashcan <git@tashcan.com>
2024-05-01 08:25:56 -0700

    Initial code to play with captston for arm

--- src/detour/aarch64/aarch64.cc
+++ src/detour/aarch64/aarch64.cc
@@ -6,6 +6,13 @@
 
 #include <asmjit/a64.h>
 #include <asmjit/asmjit.h>
+#include <capstone/capstone.h>
+
+#if SPUD_OS_APPLE
+#include <mach/mach_init.h>
+#include <mach/mach_vm.h>
+#include <mach/vm_map.h>
+#endif
 
 #include <array>
 #include <cstdint>
@@ -45,8 +52,47 @@ std::tuple<relocation_info, size_t> collect_relocations(uintptr_t address,
 
   relocation_info relocation_info;
 
+#if SPUD_OS_APPLE
   // Fill
+  csh handle;
+  cs_insn *insn;
+  vm_size_t vmsize = 0;
+  vm_address_t addr = (vm_address_t)address;
+  vm_region_basic_info_data_64_t info;
+  mach_msg_type_number_t info_count = VM_REGION_BASIC_INFO_COUNT_64;
+  memory_object_name_t object;
 
+  kern_return_t status =
+      vm_region_64(mach_task_self(), &addr, &vmsize, VM_REGION_BASIC_INFO_64,
+                   (vm_region_info_t)&info, &info_count, &object);
+
+  printf("Region 0x%" PRIu64 "(0x%" PRIu64 ") S: 0x%" PRIu64 " \n", address, addr, vmsize - (address- addr));
+  cs_open(CS_ARCH_ARM64, CS_MODE_LITTLE_ENDIAN, &handle);
+  auto count = cs_disasm(handle, reinterpret_cast<const uint8_t *>(address), vmsize - (address - addr),
+                         address, 0, &insn);
+
+  // Find the first branch instruction
+  //
+  // using captsone find all instructions that are rip relative and store the
+  // offset of the instruction in the relocation_info
+  //
+  for (size_t j = 0; j < count; j++) {
+    cs_detail *detail = insn[j].detail;
+    if (detail == nullptr) {
+        continue;
+    }
+    cs_arm64 &arm64 = detail->arm64;
+    const auto opcount = arm64.op_count;
+    for (int k = 0; k < opcount; k++) {
+      printf("%d\n", k);
+      if (arm64.operands[k].type == ARM64_OP_MEM) {
+        printf("\t%s\n", insn[j].op_str);
+        break;
+      }
+    }
+  }
+
+#endif
   return {relocation_info, jump_size};
 }
 
